1798bbc1a757d094206f7ada5f6fa4fb9de1d9df
Clara <clarazammar4@gmail.com>
2026-02-11 19:13:11 +0200

    Tie sources to API

--- src/features/automation/components/create/SourceCondition.tsx
+++ src/features/automation/components/create/SourceCondition.tsx
@@ -1,10 +1,12 @@
 import { View } from "react-native";
 import { conditionStyles } from "@/src/features/automation/styles/conditionStyles";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TSelectedItem } from "react-native-input-select/src/types/index.types";
 import AppDropdown from "@/src/shared/components/AppDropdown";
 import ConditionHeader from "@/src/features/automation/components/create/ConditionHeader";
+import { SourceDTO } from "@/src/features/automation/types/SourceDTO";
+import { api } from "@/src/api/api";
 
 interface SourceConditionProps {
   powerSourceId: number;
@@ -19,13 +21,40 @@ export default function SourceCondition({
   powerSourceId,
   editPolicy,
 }: SourceConditionProps) {
-  const sources = [
-    { label: "EDL", value: "EDL" },
-    { label: "Generator", value: "Generator" },
-  ]; // TODO: replace the sources with the ones from the api
+  const [sources, setSources] = useState<SourceDTO[]>([]);
   const [selectedSource, setSelectedSource] = useState<
-    TSelectedItem | TSelectedItem[]
-  >(sources[0].value);
+    TSelectedItem | TSelectedItem[] | null
+  >(powerSourceId);
+
+  // Get power sources
+  const getPowerSources = async () => {
+    try {
+      const res = await api.get<{ sources: SourceDTO[] }>("/mains/sources");
+      setSources(res.data.sources);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    void getPowerSources();
+  }, []);
+
+  // Set a default source value
+  useEffect(() => {
+    if (sources.length !== 0 && selectedSource === null) {
+      setSelectedSource(sources[0].id);
+    }
+  }, [sources]);
+
+  // Change the current power source id
+  useEffect(() => {
+    editPolicy(Number(selectedSource));
+  }, [selectedSource]);
+
+  if (sources.length === 0 && selectedSource === null) {
+    return;
+  }
 
   return (
     <View style={conditionStyles.container}>
@@ -35,9 +64,12 @@ export default function SourceCondition({
         onDelete={() => editPolicy(null)}
       />
       <AppDropdown
-        options={sources}
+        options={sources.map((source) => ({
+          value: source.id,
+          label: source.name,
+        }))}
         setOptions={setSelectedSource}
-        selectedOption={selectedSource}
+        selectedOption={selectedSource as TSelectedItem}
         dropdownStyle={{ width: 220 }}
         dropdownContainerStyle={{ width: 220, flex: 0 }}
       />
---
+++ src/features/automation/types/SourceDTO.ts
@@ -0,0 +1,6 @@
+export interface SourceDTO {
+  id: number;
+  name: string;
+  maxCapacity: number;
+  householdId: number;
+}
