851e2bf29e99a183a2d529f0c75f3b0c4ed2ff98
Clara <clarazammar4@gmail.com>
2026-03-09 22:20:50 +0200

    Make expo notifications work - most of the code will be changed (sent from the backend instead)

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -13,6 +13,8 @@ import {
 } from "@expo-google-fonts/poppins";
 import { Providers } from "@/src/providers/Providers";
 import * as WebBrowser from "expo-web-browser";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
 
 WebBrowser.maybeCompleteAuthSession({
   skipRedirectCheck: false,
@@ -23,6 +25,11 @@ import ChatbotButton from "@/src/features/chatbot/components/ChatbotButton";
 import { useState } from "react";
 import ChatbotModal from "@/src/features/chatbot/components/ChatbotModal";
 import { useAuth } from "@/src/auth/useAuth";
+import {
+  registerForPushNotificationsAsync,
+  schedulePushNotification,
+} from "@/src/features/notifications/utils/notificationsHelper";
+import { Platform } from "react-native";
 
 export default function RootLayout() {
   useFonts({
@@ -48,6 +55,45 @@ function RootLayoutContent() {
   const [showChat, setShowChat] = useState(false);
   const { state } = useAuth(); // to be able to use it, the wrapper should be around this component
   const { isSignedIn } = state;
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [channels, setChannels] = useState<Notifications.NotificationChannel[]>(
+    [],
+  ); // for android only
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >(undefined);
+
+  useEffect(() => {
+    if (!isSignedIn) {
+      return;
+    }
+
+    registerForPushNotificationsAsync().then(
+      (token) => token && setExpoPushToken(token),
+    );
+
+    if (Platform.OS === "android") {
+      Notifications.getNotificationChannelsAsync().then((value) =>
+        setChannels(value ?? []),
+      );
+    }
+
+    const notificationListener = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        setNotification(notification);
+      },
+    );
+
+    const responseListener =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      }); // this is how apps navigate to a screen after tapping a notification
+
+    return () => {
+      notificationListener.remove();
+      responseListener.remove();
+    };
+  }, [isSignedIn]);
 
   return (
     <>
@@ -60,6 +106,9 @@ function RootLayoutContent() {
       {isSignedIn && (
         <>
           <ChatbotButton onPress={() => setShowChat(true)} />
+          {/*<ChatbotButton*/}
+          {/*  onPress={async () => await schedulePushNotification()}*/}
+          {/*/>*/}
           <ChatbotModal isVisible={showChat} setIsVisible={setShowChat} />
         </>
       )}
---
+++ src/features/notifications/utils/notificationsHelper.tsx
@@ -0,0 +1,78 @@
+import { Platform } from "react-native";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
+
+export async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: "Here is the notification body",
+      data: { data: "goes here", test: { test1: "more data" } },
+    },
+    trigger: {
+      type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+      seconds: 2,
+    },
+  });
+}
+
+export async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("myNotificationChannel", {
+      name: "A channel is needed for the permissions prompt to appear",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+
+    try {
+      const projectId =
+        Constants?.expoConfig?.extra?.eas?.projectId ??
+        Constants?.easConfig?.projectId;
+
+      if (!projectId) {
+        throw new Error("Project ID not found");
+      }
+
+      token = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId,
+        })
+      ).data;
+      console.log(token);
+    } catch (e) {
+      token = `${e}`;
+    }
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  return token;
+}
